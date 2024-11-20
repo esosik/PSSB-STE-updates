e826f5b14256ac23dbe4ceb86efde12658b050dd
Beth Sosik <esosik@kingcounty.gov>
2024-11-20 09:34:51 -0800

    Updated mapping_update_screening_tool.R and current mapping table used by PSSB

--- mapping_update_screening_tool.R
+++ mapping_update_screening_tool.R
@@ -2,11 +2,15 @@ library(openxlsx)
 
 setwd("C:/Users/esosik/King County/DNRP - Science TCA Unit - Bug Monitoring/PSSB issues/STE_attrib_updates")
 
-PSSBmapping<-read.xlsx("STE_Mapping_Atts_Draft.xlsx", detectDates = T, sheet="Draft Mapping")#load the mapping used by PSSB
+# PSSBmapping<-read.xlsx("STE_Mapping_Atts_Draft.xlsx", detectDates = T, sheet="Draft Mapping")#load the mapping used by PSSB
+PSSBmapping<-read.xlsx("PSSB_mapping.xlsx")#load the mapping used by PSSB
 
-BCGmapping<-read.csv("ORWA_TaxaTranslator_20240417.csv")# load the latest BCG mapping
+BCGmapping<-read.csv("ORWA_TaxaTranslator_20240619.csv")# load the latest BCG mapping
 
-mapping<-merge(PSSBmapping, BCGmapping, by.x=c("Taxon"), by.y=c("Taxon"), all=T) #merge the two mappings together, keep all items in both dataframes
+mapping<-merge(PSSBmapping, BCGmapping, by.x=c("Alternate.Name"), by.y=c("Taxon_orig"), all=T) #merge the two mappings together, keep all items in both dataframes
+
+subset(mapping, is.na(OTU_MetricCalc))
+test<-subset(mapping, is.na(Preferred.Name)&Alternate.Name!=OTU_MetricCalc&OTU_MetricCalc!="DNI")
 
 ##this function binds the textfiles from the taxa downloads from PSSB into one dataframe
 taxaBind <- function(file.path) {
@@ -29,22 +33,24 @@ PSSB_taxa<-unique(raw[,c(28, 29, 48:69)])
 PSSB_taxa[is.na(PSSB_taxa)]<-""
 PSSB_taxa<-unique(PSSB_taxa) #we're generating a list of all taxa in PSSB samples
 
-mapping<-merge(mapping, PSSB_taxa, by.x=c("Taxon"), by.y=c("Taxon"), all.y=T) #merge the PSSB taxa list iwth the mapping dataframe, and limit the output to only taxa in the PSSB taxa list.
+mapping<-merge(mapping, PSSB_taxa, by.x=c("Alternate.Name"), by.y=c("Taxon"), all.y=T) #merge the PSSB taxa list iwth the mapping dataframe, and limit the output to only taxa in the PSSB taxa list.
 
-missingBCGmapping<-subset(mapping, is.na(OTU_MetricCalc.y), select="Taxon")##these are taxa in PSSB samples that have not been translated by the BCG working group. 
+missingBCGmapping<-subset(mapping, is.na(OTU_MetricCalc), select="Alternate.Name")##these are taxa in PSSB samples that have not been translated by the BCG working group. 
+write.csv(missingBCGmapping, "Missing_from_BCG_taxa_translator.csv")
 
-missingPSSBmapping<-subset(mapping, is.na(OTU_MetricCalc.x), select="Taxon")##these are taxa in PSSB samples that don't have a mapping assigned in PSSB. 
+missingPSSBmapping<-subset(mapping, is.na(Preferred.Name)&Alternate.Name!=OTU_MetricCalc&OTU_MetricCalc!="DNI")##these are taxa in PSSB samples that don't have a mapping assigned in PSSB. 
+write.csv(missingPSSBmapping, "Missing_from_PSSB_mapping_table.csv")
 
 ###because there may be overlap between the last two, this next section will parse out the differences.
 
-missingPSSBmappingBCGexists<-missingPSSBmapping$Taxon[!missingPSSBmapping$Taxon %in% missingBCGmapping$Taxon] ##these are taxa in PSSB samples that have a BCG translation, but do not have a mapping assigned in PSSB. Update the PSSB mapping to include these. 
+missingPSSBmappingBCGexists<-missingPSSBmapping$Alternate.Name[!missingPSSBmapping$Alternate.Name %in% missingBCGmapping$Alternate.Name] ##these are taxa in PSSB samples that have a BCG translation, but do not have a mapping assigned in PSSB. Update the PSSB mapping to include these. 
 
-missingBCG_PSSBmappingexists<-missingBCGmapping$Taxon[!missingBCGmapping$Taxon %in% missingPSSBmapping$Taxon]##these are taxa in PSSB samples that do not have a BCG translation, but do have a mapping assigned in PSSB. Probably a result of BAS editing some taxa names in PSSB for clarity, formatting and accuracy (i.e. Rhabdomastix (Rhabdomastix (Rhabdomastix)) was edited in PSSB taxonomy to simply Rhabdomastix (Rhabdomastix)). Let Sean know about these name changes in PSSB so he can update the BCG translation table with the edited names. 
+# missingBCG_PSSBmappingexists<-missingBCGmapping$Alternate.Name[!missingBCGmapping$Alternate.Name %in% missingPSSBmapping$Alternate.Name]##these are taxa in PSSB samples that do not have a BCG translation, but do have a mapping assigned in PSSB. Probably a result of BAS editing some taxa names in PSSB for clarity, formatting and accuracy (i.e. Rhabdomastix (Rhabdomastix (Rhabdomastix)) was edited in PSSB taxonomy to simply Rhabdomastix (Rhabdomastix)). Let Sean know about these name changes in PSSB so he can update the BCG translation table with the edited names. 
 
-missingBCGPSSB<-missingBCGmapping$Taxon[missingBCGmapping$Taxon %in% missingPSSBmapping$Taxon]##these are taxa in PSSB samples that do not have a BCG translation, and do not have a mapping assigned in PSSB. Send these to Sean Sullivan once a year for updating the BCG translation tables.
+missingBCGPSSB<-missingBCGmapping$Alternate.Name[missingBCGmapping$Alternate.Name %in% missingPSSBmapping$Alternate.Name]##these are taxa in PSSB samples that do not have a BCG translation, and do not have a mapping assigned in PSSB. Send these to Sean Sullivan once a year for updating the BCG translation tables.
 
-missingBCGmapping$Taxon[missingPSSBmapping$Taxon %in% missingBCGmapping$Taxon] ###this output should be identical to the object missingBCGPSSB
+# missingBCGmapping$Alternate.Name[missingPSSBmapping$Alternate.Name %in% missingBCGmapping$Alternate.Name] ###this output should be identical to the object missingBCGPSSB
 
 ###this next section checks to see if existing mappings have changed since the last version of the BCG table.
 
-changedmapping<-mapping[which(mapping$OTU_MetricCalc.x !=mapping$OTU_MetricCalc.y),c("Taxon", "OTU_MetricCalc.x", "OTU_MetricCalc.y")] ##in many cases, the BCG translates taxa to "DNI" meaning "Do Not Include". But since these taxa are not explicitly excluded in the PSSB calculations, we created mappings for these in PSSB based simply on copying the taxa name into the mapping column. BAS also did not wish to enter "Polycentropus sensu lato" as a new taxa in PSSB since "Polycentropus" already existed (and truly implementing "sensu lato" qualifications in PSSB isn't feasible). So BAS kept the mapping to "Polycentropus". Any other differences should be examined, and the PSSB mappings should be updated to reflect the current BCG translations. Check taxa attributes as well to see if new taxa names should have attributes assigned from old taxa names. 
+changedmapping<-mapping[which(mapping$Preferred.Name !=mapping$OTU_MetricCalc),c("Alternate.Name", "Preferred.Name", "OTU_MetricCalc")] ##in many cases, the BCG translates taxa to "DNI" meaning "Do Not Include". But since these taxa are not explicitly excluded in the PSSB calculations, we created mappings for these in PSSB based simply on copying the taxa name into the mapping column. BAS also did not wish to enter "Polycentropus sensu lato" as a new taxa in PSSB since "Polycentropus" already existed (and truly implementing "sensu lato" qualifications in PSSB isn't feasible). So BAS kept the mapping to "Polycentropus". Any other differences should be examined, and the PSSB mappings should be updated to reflect the current BCG translations. Check taxa attributes as well to see if new taxa names should have attributes assigned from old taxa names. 
